3cb10f84c855ba7919eda9844d19bc0b7698ffb0
alumno Secundaria <al3201@cedi.local>
2018-10-01 08:43:24 -0500

    Nueva pagina de atras

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HistoriaPage } from '../historia/historia';
+import { DibujoPage } from '../dibujo/dibujo';
 
 @Component({
   selector: 'page-home',
@@ -8,11 +9,13 @@ import { HistoriaPage } from '../historia/historia';
 })
 export class HomePage {
 historia = HistoriaPage;
+dibujo = DibujoPage;
   constructor(public navCtrl: NavController) {
 
   }
 
   clickHistoria(){
     this.navCtrl.push(this.historia);
+    this.navCtrl.push(this.dibujo);
   }
 }
